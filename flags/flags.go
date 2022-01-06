398762f742b3c00a058af034032835a2b2ccbf50
Berny Linville <bernylinville@devopsthink.org>
2022-01-06 15:05:00 +0800

    add flags

---
+++ flags/flags.go
@@ -0,0 +1,9 @@
+package flags
+
+// Flags is the container for command line flag data
+type Flags struct{}
+
+// NewFlags creates an instance of Flags
+func NewFlags() *Flags {
+	return &Flags{}
+}
--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"log"
+
+	"github.com/bernylinville/learn-wtf/flags"
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile) // 设置log标准输出格式
+
+	// paras and handle flags
+	flags := flags.NewFlags()
+	flags.Parse()
 }
